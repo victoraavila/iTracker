eff2318c402f5bfe37a551cfe32c174d8505c3df
Víctor Ávila <victoraavila2000@gmail.com>
2024-04-05 00:05:49 -0300

    Initial Commit

---
+++ iTracker/iTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTrackerApp.swift
+//  iTracker
+//
+//  Created by Víctor Ávila on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct iTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
